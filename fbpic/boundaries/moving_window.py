29d10fcaca6641872f879c3847464fe58642ec09
MKirchen <manuel.kirchen@desy.de>
2020-02-03 13:09:35 +0100

    Added argtypes to moving window

--- fbpic/boundaries/moving_window.py
+++ fbpic/boundaries/moving_window.py
@@ -8,6 +8,7 @@ It defines the structure necessary to implement the moving window.
 from fbpic.utils.threading import njit_parallel, prange
 # Check if CUDA is available, then import CUDA functions
 from fbpic.utils.cuda import cuda_installed
+from numba import int64, complex128
 if cuda_installed:
     from fbpic.utils.cuda import cuda, cuda_tpb_bpg_2d
 
@@ -240,7 +241,7 @@ def shift_spect_array_cpu( field_array, shift_factor, n_move ):
 
 if cuda_installed:
 
-    @cuda.jit
+    @cuda.jit(argtypes=[complex128[:,:],complex128[:],int64])
     def shift_spect_array_gpu( field_array, shift_factor, n_move ):
         """
         Shift the field 'field_array' by n_move cells on the GPU.
