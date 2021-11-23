a22c540666a7d0b445cd235481f71a6e1b90ea99
Manuel Kirchen <manuel.kirchen@desy.de>
2021-11-23 14:24:52 +0100

    Fix id location

--- fbpic/openpmd_diag/particle_diag.py
+++ fbpic/openpmd_diag/particle_diag.py
@@ -393,7 +393,7 @@ class ParticleDiagnostic(OpenPMDDiagnostic) :
             select_array = np.logical_and(subsampling_array,select_array)
         # id selector
         if self.ids is not None :
-            ids_array = getattr(species, "id")
+            ids_array = getattr(species.tracker, "id")
             select_array = np.logical_and(ids_array == self.ids,select_array)
 
         # Apply the rules successively
